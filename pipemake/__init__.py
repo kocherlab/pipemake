754f5df6719b97af0ad769d7f8400694f29748e5
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2024-05-24 17:05:57 -0400

    version

--- pipemake/__init__.py
+++ pipemake/__init__.py
@@ -2,7 +2,7 @@ import sys
 
 # Basic Information
 __name__ = "pipemake"
-__version__ = "0.6"
+__version__ = "0.7"
 __summary__ = "Pipemake: A pipeline creation tool using Snakemake"
 __url__ = "https://github.com/kocherlab/pipemake"
 __code__ = "https://github.com/kocherlab/pipemake"
