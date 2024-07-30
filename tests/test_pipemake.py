75629c003f85d3320cde50075a0e42d9c07d23d6
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2024-07-30 15:49:38 -0400

    bug fix for table test

--- tests/test_pipemake.py
+++ tests/test_pipemake.py
@@ -13,7 +13,7 @@ from pipemake.pipemake import *
         'tests/files/wildcardIO/{sample}_{read}.fq.gz',
     ],
 )
-def test_pipemake_main (wildcard_str):
+def test_pipemake_main_wildcard_wo_error (wildcard_str):
     test_dir = tempfile.mkdtemp()
     os.environ['PM_SNAKEMAKE_DIR'] = 'tests/files/snakemakeIO'
 
@@ -40,3 +40,58 @@ def test_pipemake_main (wildcard_str):
     assert os.path.isfile(f'{workflow_prefix}/pipemake/modules/fastq_filter_fastp.smk')
     assert os.path.isdir(f'{workflow_prefix}/pipemake/modules')
     assert os.path.isdir(f'{workflow_prefix}/FASTQ/Unfiltered')
+
+@pytest.mark.parametrize(
+    "table_str",
+    [
+        'tests/files/seqIO/test_table.tsv',
+    ],
+)
+def test_pipemake_main_table_wo_error (table_str):
+    test_dir = tempfile.mkdtemp()
+    os.environ['PM_SNAKEMAKE_DIR'] = 'tests/files/snakemakeIO'
+
+    # Assign the workflor prefix
+    workflow_prefix = os.path.join(test_dir, 'test_workflow')
+
+    # Assign the command line arguments
+    test_cmd = ['', 'fastq-filter', '--fastq-table', table_str, '--workflow-prefix', workflow_prefix, '--resource-yml']
+
+    # Run pipemake with the test command
+    with unittest.mock.patch('sys.argv', test_cmd):
+        main()
+
+    # Check if the workflow files were created
+    assert os.path.isfile(f'{workflow_prefix}.smk')
+    assert os.path.isfile(f'{workflow_prefix}.yml')
+    assert os.path.isdir(f'{workflow_prefix}')
+    assert os.path.isfile(f'{workflow_prefix}/pipemake/pipeline.log')
+    assert os.path.isdir(f'{workflow_prefix}/pipemake')
+    assert os.path.isfile(f'{workflow_prefix}/pipemake/backups/test_workflow.smk.bkp')
+    assert os.path.isfile(f'{workflow_prefix}/pipemake/backups/test_workflow.yml.bkp')
+    assert os.path.isfile(f'{workflow_prefix}/pipemake/backups/test_workflow.resources.yml.bkp')
+    assert os.path.isdir(f'{workflow_prefix}/pipemake/backups')
+    assert os.path.isfile(f'{workflow_prefix}/pipemake/modules/fastq_filter_fastp.smk')
+    assert os.path.isdir(f'{workflow_prefix}/pipemake/modules')
+    assert os.path.isdir(f'{workflow_prefix}/FASTQ/Unfiltered')
+
+@pytest.mark.parametrize(
+    "table_str",
+    [
+        'tests/files/seqIO/test_table2.tsv',
+    ],
+)
+def test_pipemake_main_table_w_error (table_str):
+    test_dir = tempfile.mkdtemp()
+    os.environ['PM_SNAKEMAKE_DIR'] = 'tests/files/snakemakeIO'
+
+    # Assign the workflor prefix
+    workflow_prefix = os.path.join(test_dir, 'test_workflow')
+
+    # Assign the command line arguments
+    test_cmd = ['', 'fastq-filter', '--fastq-table', table_str, '--workflow-prefix', workflow_prefix, '--resource-yml']
+
+    # Run pipemake with the test command
+    with unittest.mock.patch('sys.argv', test_cmd):
+        with pytest.raises(Exception):
+            main()
