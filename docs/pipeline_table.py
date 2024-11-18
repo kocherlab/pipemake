e715dd46da408b580c67d8baa9ed8a8b633f580f
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2024-11-18 17:40:19 -0500

    sort the docs table

--- docs/pipeline_table.py
+++ docs/pipeline_table.py
@@ -19,6 +19,9 @@ with open(pipeline_table_filename, "w") as table_file:
     # Assign the pipeline directory
     pipeline_dir = os.path.join("pipelines", "configs")
 
+    # Create a list to store the pipeline text
+    pipeline_list = []
+
     # Loop the pipeline files in the pipeline directory
     for pipeline_filename in os.listdir(pipeline_dir):
         if not pipeline_filename.endswith(".yml") and not pipeline_filename.endswith(
@@ -33,13 +36,20 @@ with open(pipeline_table_filename, "w") as table_file:
         with open(pipeline_path, "r") as file:
             pipeline_data = yaml.safe_load(file)
 
-        # Write the pipeline data to the pipelines table file
-        table_file.write(
+        # Write the pipeline data to the pipelines list
+        pipeline_list.append(
             f'{pipeline_data["pipeline"]},{pipeline_data["parser"]["help"]}\n'
         )
 
         # Report the pipeline file that was parsed
         print(f"Pipeline file parsed: {pipeline_path}")
+
+    # Sort the pipeline list
+    pipeline_list.sort()
+
+    # Write the pipeline list to the pipelines table file
+    for pipeline_text in pipeline_list:
+        table_file.write(pipeline_text)
 
 # Report the pipelines table file that was created
 print(f"Pipelines table created: {pipeline_table_filename}")
