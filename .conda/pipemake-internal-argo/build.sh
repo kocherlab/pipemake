3aab5e38516429eb6ecc673552d6fd5165eb6d99
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2024-11-20 11:45:35 -0500

    Adding internal package

---
+++ .conda/pipemake-internal-argo/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+pipeline_dir="/Genomics/kocherlab/lab/Pipelines/pipemake/pipelines"
+
+mkdir -p "${PREFIX}/etc/conda/activate.d"
+echo "export PM_SNAKEMAKE_DIR=${pipeline_dir}" > "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
+
+mkdir -p "${PREFIX}/etc/conda/deactivate.d"
+echo "unset PM_SNAKEMAKE_DIR=${pipeline_dir}" > "${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh"
+
+python -m pip install --no-deps --ignore-installed .
