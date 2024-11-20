2ffe94bbab1f086db47b70dccc0b0cde36b594ac
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2024-11-20 12:52:30 -0500

    fix append call

--- .conda/pipemake-internal-argo/build.sh
+++ .conda/pipemake-internal-argo/build.sh
@@ -5,11 +5,11 @@ image_dir="/Genomics/kocherlab/lab/Pipelines/images"
 
 mkdir -p "${PREFIX}/etc/conda/activate.d"
 echo "export PM_SNAKEMAKE_DIR=${pipeline_dir}" > "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
-echo "export PM_SINGULARITY_DIR=${image_dir}" > "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
+echo "export PM_SINGULARITY_DIR=${image_dir}" >> "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
 
 
 mkdir -p "${PREFIX}/etc/conda/deactivate.d"
 echo "unset PM_SNAKEMAKE_DIR=${pipeline_dir}" > "${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh"
-echo "unset PM_SINGULARITY_DIR=${image_dir}" > "${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh"
+echo "unset PM_SINGULARITY_DIR=${image_dir}" >> "${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh"
 
 python -m pip install --no-deps --ignore-installed .
