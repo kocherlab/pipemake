0e6dd1177d82fd71bccf8c23c6207d97090de82a
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2026-03-25 15:25:21 -0400

    add internal della support

---
+++ .conda/pipemake-internal-della/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+pipeline_dir="/scratch/gpfs/SKOCHER/lab/pipelines/pipemake/pipelines"
+image_dir="/scratch/gpfs/SKOCHER/lab/pipelines/images"
+
+mkdir -p "${PREFIX}/etc/conda/activate.d"
+echo "export PM_SNAKEMAKE_DIR=${pipeline_dir}" > "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
+echo "export PM_SINGULARITY_DIR=${image_dir}" >> "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
+
+mkdir -p "${PREFIX}/etc/conda/deactivate.d"
+echo "unset PM_SNAKEMAKE_DIR" > "${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh"
+echo "unset PM_SINGULARITY_DIR" >> "${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh"
+
+python -m pip install --no-deps --ignore-installed .
